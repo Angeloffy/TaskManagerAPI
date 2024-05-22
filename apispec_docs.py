810962b7398486dc78177cc30019fe62b577f555
angeloffy <angeloffy.work@gmail.com>
2024-05-22 17:29:53 +0300

    feat: swagger

---
+++ apispec_docs.py
@@ -0,0 +1,124 @@
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
+from schemas import TaskSchema, TaskSchema_update
+
+
+def get_apispec(app):
+    spec = APISpec(
+        title="Task API",
+        version="1.0.0",
+        openapi_version="3.0.3",
+        plugins=[MarshmallowPlugin()],
+    )
+    with app.test_request_context():
+        spec.components.schema("Task", schema=TaskSchema)
+        spec.components.schema("TaskSchema_update", schema=TaskSchema_update)
+
+    spec.path(
+        path="/tasks",
+        operations={
+            "post": {
+                "responses": {
+                    "201": {"description": "Created", "schema": {"$ref": "#/components/schemas/Task"}}
+                },
+                "summary": "Create a new task",
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {"$ref": "#/components/schemas/Task"}
+                        }
+                    }
+                }
+            }
+        }
+    )
+
+    spec.path(
+        path="/tasks/{task_id}",
+        parameters=[
+            {
+                "name": "task_id",
+                "in": "path",
+                "required": True,
+                "schema": {"type": "integer"},
+                "description": "ID of the task"
+            }
+        ],
+        operations={
+            "get": {
+                "responses": {
+                    "200": {"description": "OK", "schema": {"$ref": "#/components/schemas/Task"}},
+                    "404": {"description": "Task not found"}
+                },
+                "summary": "Get a task by ID"
+            }
+        }
+    )
+
+    spec.path(
+        path="/tasks",
+        operations={
+            "get": {
+                "responses": {
+                    "200": {"description": "OK",
+                            "schema": {"type": "array", "items": {"$ref": "#/components/schemas/Task"}}}
+                },
+                "summary": "Get all tasks"
+            }
+        }
+    )
+
+    spec.path(
+        path="/tasks/{task_id}",
+        parameters=[
+            {
+                "name": "task_id",
+                "in": "path",
+                "required": True,
+                "schema": {"type": "integer"},
+                "description": "ID of the task to delete"
+            }
+        ],
+        operations={
+            "delete": {
+                "responses": {
+                    "200": {"description": "OK"},
+                    "404": {"description": "Task not found"}
+                },
+                "summary": "Delete a task by ID"
+            }
+        }
+    )
+
+    spec.path(
+        path="/tasks/{task_id}",
+        parameters=[
+            {
+                "name": "task_id",
+                "in": "path",
+                "required": True,
+                "schema": {"type": "integer"},
+                "description": "ID of the task"
+            }
+        ],
+        operations={
+            "put": {
+                "responses": {
+                    "200": {"description": "OK", "schema": {"$ref": "#/components/schemas/Task"}},
+                    "400": {"description": "Bad Request"},
+                    "404": {"description": "Task not found"}
+                },
+                "summary": "Update a task by ID",
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {"$ref": "#/components/schemas/TaskSchema_update"}
+                        }
+                    }
+                }
+            }
+        }
+    )
+
+    return spec
--- app.py
+++ app.py
@@ -1,20 +1,47 @@
+from flask import jsonify
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
+from apispec_docs import get_apispec
 from database.engine import create_tables
 from routers import create_task, get_tasksID, get_tasks, update_tasks, delete_tasks
 from middleware import db_session_middleware
 
+# Создание экземпляра Flask приложения
 app = Flask(__name__)
 
-
+# Регистрация blueprint для маршрутов
 app.register_blueprint(create_task.bp)
 app.register_blueprint(get_tasksID.bp)
 app.register_blueprint(get_tasks.bp)
 app.register_blueprint(update_tasks.bp)
 app.register_blueprint(delete_tasks.bp)
 
+# Подключение middleware для работы с сессией базы данных
 db_session_middleware(app)
 
 create_tables()
+
+
+# Маршрут для доступа к спецификации Swagger
+@app.route('/swagger')
+def swagger_spec():
+    spec = get_apispec(app).to_dict()
+    return jsonify(spec)
+
+
+SWAGGER_URL = '/docs'
+API_URL = '/swagger'
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Tasks API'
+    }
+)
+
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(debug=True)
